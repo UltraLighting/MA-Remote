96cbc1ba53ab47e51b9e438cfcaf239c1e8da787
Phil <philip.schaefer0@gmail.com>
2022-05-10 01:25:15 +0200

    [add] testfunction

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export default function printMsg() {
   console.log("This is a message from the demo package");
 }
+
+export function too() {
+  console.log("This is ANOTHER message from the demo package");
+}
